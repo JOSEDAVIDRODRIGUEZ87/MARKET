70b9c8060690d87db78153fe2782bcec9dced555
DavidRodriguez <david.rodriguez@transmilenio.gov.co>
2026-03-08 23:19:42 -0500

    configuracion para uso de base de datos

---
+++ sales/MarketProduction.Infrastructure/Migrations/20260309040056_InitialCreate.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MarketProduction.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CategoryName = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Picture = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerID = table.Column<string>(type: "character varying(5)", maxLength: 5, nullable: false),
+                    CompanyName = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false),
+                    ContactName = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: true),
+                    ContactTitle = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: true),
+                    Address = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: true),
+                    City = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: true),
+                    Region = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: true),
+                    PostalCode = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: true),
+                    Country = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: true),
+                    Phone = table.Column<string>(type: "character varying(24)", maxLength: 24, nullable: true),
+                    Fax = table.Column<string>(type: "character varying(24)", maxLength: 24, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    LastName = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    FirstName = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    Title = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: true),
+                    TitleOfCourtesy = table.Column<string>(type: "character varying(25)", maxLength: 25, nullable: true),
+                    BirthDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    HireDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Address = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: true),
+                    City = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: true),
+                    Region = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: true),
+                    PostalCode = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: true),
+                    Country = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: true),
+                    HomePhone = table.Column<string>(type: "character varying(24)", maxLength: 24, nullable: true),
+                    Extension = table.Column<string>(type: "character varying(4)", maxLength: 4, nullable: true),
+                    Photo = table.Column<string>(type: "text", nullable: true),
+                    Notes = table.Column<string>(type: "text", nullable: true),
+                    ReportsTo = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeID);
+                    table.ForeignKey(
+                        name: "FK_Employees_Employees_ReportsTo",
+                        column: x => x.ReportsTo,
+                        principalTable: "Employees",
+                        principalColumn: "EmployeeID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shippers",
+                columns: table => new
+                {
+                    ShipperID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CompanyName = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false),
+                    Phone = table.Column<string>(type: "character varying(24)", maxLength: 24, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shippers", x => x.ShipperID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Suppliers",
+                columns: table => new
+                {
+                    SupplierID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CompanyName = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false),
+                    ContactName = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: true),
+                    ContactTitle = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: true),
+                    Address = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: true),
+                    City = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: true),
+                    Region = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: true),
+                    PostalCode = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: true),
+                    Country = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: true),
+                    Phone = table.Column<string>(type: "character varying(24)", maxLength: 24, nullable: true),
+                    Fax = table.Column<string>(type: "character varying(24)", maxLength: 24, nullable: true),
+                    HomePage = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suppliers", x => x.SupplierID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CustomerID = table.Column<string>(type: "character varying(5)", nullable: false),
+                    EmployeeID = table.Column<int>(type: "integer", nullable: true),
+                    OrderDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    RequiredDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    ShippedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    ShipVia = table.Column<int>(type: "integer", nullable: true),
+                    Freight = table.Column<decimal>(type: "numeric(18,2)", nullable: true),
+                    ShipName = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: true),
+                    ShipAddress = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: true),
+                    ShipCity = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: true),
+                    ShipRegion = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: true),
+                    ShipPostalCode = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: true),
+                    ShipCountry = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: true),
+                    ShipperID = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderID);
+                    table.ForeignKey(
+                        name: "FK_Orders_Customers_CustomerID",
+                        column: x => x.CustomerID,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Orders_Employees_EmployeeID",
+                        column: x => x.EmployeeID,
+                        principalTable: "Employees",
+                        principalColumn: "EmployeeID");
+                    table.ForeignKey(
+                        name: "FK_Orders_Shippers_ShipperID",
+                        column: x => x.ShipperID,
+                        principalTable: "Shippers",
+                        principalColumn: "ShipperID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ProductName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    CategoryID = table.Column<int>(type: "integer", nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    UnitsInStock = table.Column<short>(type: "smallint", nullable: true),
+                    Discontinued = table.Column<bool>(type: "boolean", nullable: false),
+                    QuantityPerUnit = table.Column<string>(type: "text", nullable: true),
+                    UnitsOnOrder = table.Column<short>(type: "smallint", nullable: true),
+                    ReorderLevel = table.Column<short>(type: "smallint", nullable: true),
+                    SupplierID = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductID);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryID",
+                        column: x => x.CategoryID,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Products_Suppliers_SupplierID",
+                        column: x => x.SupplierID,
+                        principalTable: "Suppliers",
+                        principalColumn: "SupplierID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    OrderID = table.Column<int>(type: "integer", nullable: false),
+                    ProductID = table.Column<int>(type: "integer", nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    Quantity = table.Column<short>(type: "smallint", nullable: false),
+                    Discount = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => new { x.OrderID, x.ProductID });
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Orders_OrderID",
+                        column: x => x.OrderID,
+                        principalTable: "Orders",
+                        principalColumn: "OrderID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Products_ProductID",
+                        column: x => x.ProductID,
+                        principalTable: "Products",
+                        principalColumn: "ProductID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_ReportsTo",
+                table: "Employees",
+                column: "ReportsTo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_ProductID",
+                table: "OrderDetails",
+                column: "ProductID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerID",
+                table: "Orders",
+                column: "CustomerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_EmployeeID",
+                table: "Orders",
+                column: "EmployeeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_ShipperID",
+                table: "Orders",
+                column: "ShipperID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryID",
+                table: "Products",
+                column: "CategoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductName",
+                table: "Products",
+                column: "ProductName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_SupplierID",
+                table: "Products",
+                column: "SupplierID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Shippers");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Suppliers");
+        }
+    }
+}
